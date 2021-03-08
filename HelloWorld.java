3206a2782a7c65b7603411f02b7af087e9bbf5f3
silverzero <ekim0010@email.cpcc.edu>
2021-03-08 11:29:23 -0500

    Update HelloWorld.java
    added my name

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,9 @@ public class HelloWorld
    public static void main(String[] args)
    {
       System.out.println("Hello World!");
+      
+      String name = "Eunyoung";
+      
+      System.out.println("by " + name);
    }
 }
